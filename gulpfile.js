9b407fe6618d8d99ad413db1dd3fc1806c516d26
Guillermo <guillermo@justthingsido.com>
2016-01-01 01:33:44 -0500

    .gulp file edit
    Critical CSS inline added

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,18 @@ var gulp        = require('gulp'),
   gulpif        = require('gulp-if'),
   jade          = require('gulp-jade'),
   minifyCSS     = require('gulp-minify-css'),
-  plumber     = require('gulp-plumber'),
+  plumber       = require('gulp-plumber'),
   sass          = require('gulp-sass'),
   sequence      = require('run-sequence'),
   gutil         = require('gulp-util'),
-  uglify        = require('gulp-uglify');
+  uglify        = require('gulp-uglify'),
+  uncss         = require('gulp-uncss'),
+  minifyHTML    = require('gulp-minify-html'),
+  replace       = require('gulp-replace'),
+  fs            = require('fs'),
+  imagemin      = require('gulp-imagemin'),
+  pngquant      = require('imagemin-pngquant'),
+  jpegtran      = require('imagemin-jpegtran');
   /**
    * Project Configuration
    * =====================
@@ -38,23 +45,49 @@ var onError     = function(err) {
 // Jekyll build
 gulp.task('jekyll-build', function (done) {
     bs.notify('Running jekyll-build');
-    return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
+    return cp.spawn('jekyll.bat', ['build','--incremental'], {stdio: 'inherit'})
         .on('close', done);
 });
 
 
 // Run jekyll build and reload browser
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', ['jekyll-build'], function (done) {
+  sequence(
+    'optimize-css',
+    'optimize-html',
+    done);
     bs.reload();
 });
 
+// Inline CSS
+gulp.task('optimize-css', function() {
+   return gulp.src('_site/assets/css/main.css')
+       .pipe(uncss({
+           html: ['_site/**/*.html'],
+           ignore: []
+       }))
+       .pipe(minifyCSS({keepBreaks: false}))
+       .pipe(gulp.dest('_site/assets/css/critical/'));
+});
 
+gulp.task('optimize-html', function() {
+    return gulp.src('_site/**/*.html')
+        .pipe(minifyHTML({
+            quotes: true
+        }))
+        .pipe(replace(/<link href=\"\/assets\/css\/critical\/main.css\"[^>]*>/, function(s) {
+            var style = fs.readFileSync('_site/assets/css/critical/main.css', 'utf8');
+            return '<style>\n' + style + '\n</style>';
+        }))
+        .pipe(gulp.dest('_site/'));
+});
 
 // Compile Jade Files
 gulp.task('jade', function() {
   gulp.src('_includes/jade/*.jade')
     .pipe(plumber({ errorHandler: onError }))
     .pipe(jade({
+      doctype: 'html',
       pretty: true
     }))
     .pipe(gulp.dest('_includes'))
@@ -87,6 +120,17 @@ gulp.task('js', function () {
 });
 
 
+gulp.task('img', function () {
+    return gulp.src('assets/img/**/*')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(imagemin({
+            progressive: false,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant(), jpegtran()]
+        }))
+        .pipe(gulp.dest('assets/img'));
+});
+
 gulp.task('browser-sync', function(done) {
     bs.init({
         server: '_site',
@@ -97,24 +141,23 @@ gulp.task('browser-sync', function(done) {
 
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch('_includes/**/*.jade', ['jade']);
+  gulp.watch('_includes/jade/*.jade', ['jade']);
   //watch for sass file changes
   gulp.watch('_sass/**/*.sass', ['sass']);
   gulp.watch('assets/css/*.sass', ['sass']);
   gulp.watch('assets/_js/*.js', ['js', 'jekyll-rebuild']);
   // Watch all html.files in root directory
   gulp.watch(['*.html', '*.md','*.yml', '_layouts/**/*.html', '_posts/**/*' ,'_includes/**/*', '_data/**/*'], ['jekyll-rebuild']);
-
+  gulp.watch('assets/_img/**/*', ['img', 'jekyll-rebuild']);  //watch for js files in assets
 });
 
 gulp.task('build', function(done) {
   sequence(
-    ['jade', 'sass','js'],
+    ['jade', 'sass','js', 'img'],
     'jekyll-rebuild',
     'browser-sync',
     done);
 });
-
 
 
 gulp.task('default', ['watch']);
