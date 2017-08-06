b4ca83b8fc5333e5b71bff03408545dcd4f69391
Guillermo Remache <guillermo@justthingsido.com>
2017-08-06 17:15:35 -0400

    Edited Resume
    Added more jobs and fixed Projects for XSS

--- assets/_js/functions.js
+++ assets/_js/functions.js
@@ -98,40 +98,40 @@ $(document).ready(function () {
 	// 	});
 	// })();
 
-	(function() {
-	  var delay = false;
+	// (function() {
+	//   var delay = false;
 
-	  $(document).on('mousewheel DOMMouseScroll', function(event) {
-	    event.preventDefault();
-	    if (delay) return;
+	//   $(document).on('mousewheel DOMMouseScroll', function(event) {
+	//     event.preventDefault();
+	//     if (delay) return;
 
-	    delay = true;
-	    setTimeout(function() {
-	      delay = false;
-	    }, 200);
+	//     delay = true;
+	//     setTimeout(function() {
+	//       delay = false;
+	//     }, 200);
 
-	    var wd = event.originalEvent.wheelDelta || - event.originalEvent.detail;
-	    var i;
-	    var t;
-	    var a = document.getElementsByTagName('section');
-	    if (wd < 0) {
-	      for (i = 0; i < a.length; i++) {
-	        t = a[i].getClientRects()[0].top;
-	        if (t >= 40) break;
-	      }
-	    } else {
-	      for (i = a.length - 1; i >= 0; i--) {
-	        t = a[i].getClientRects()[0].top;
-	        if (t < -20) break;
-	      }
-	    }
+	//     var wd = event.originalEvent.wheelDelta || - event.originalEvent.detail;
+	//     var i;
+	//     var t;
+	//     var a = document.getElementsByTagName('section');
+	//     if (wd < 0) {
+	//       for (i = 0; i < a.length; i++) {
+	//         t = a[i].getClientRects()[0].top;
+	//         if (t >= 40) break;
+	//       }
+	//     } else {
+	//       for (i = a.length - 1; i >= 0; i--) {
+	//         t = a[i].getClientRects()[0].top;
+	//         if (t < -20) break;
+	//       }
+	//     }
 
-	    $('html,body').animate({
-	      scrollTop: a[i].offsetTop
-	    });
+	//     $('html,body').animate({
+	//       scrollTop: a[i].offsetTop
+	//     });
 
-	  });
-	})();
+	//   });
+	// })();
 
 
 $(document).ready(function(){
